d715a7b60294e2d74ff383a9a409881b2dc325b1
Moises <ve11621@innovaccion.mx>
2022-10-10 15:16:26 -0500

    I finally could send a payload through wizdlr, so now I know how to do a simple request using soap. The problem was with the schema, for some reason the xsd's generated by JABX from the classes are not being correctly read by spring, and as a result, the inputs and outputs are not being created

--- src/main/java/com/springsoappractice/springsoappractice/Usuario/UsuarioController.java
+++ src/main/java/com/springsoappractice/springsoappractice/Usuario/UsuarioController.java
@@ -1,16 +1,25 @@
 package com.springsoappractice.springsoappractice.Usuario;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import io.webspring.gs_prod.GetUsuariosRequest;
+import io.webspring.gs_prod.GetUsuariosResponse;
+import io.webspring.gs_prod.Usuario;
 
 @Endpoint
 public class UsuarioController {
-   /*  @PayloadRoot(namespace = "http://localhost:8080/usuarios",localPart = "getAllUsuarios")
-    @ResponsePayload*/
+    public static final String NAMESPACE="http://webspring.io/gs-prod";
+    @PayloadRoot(namespace = NAMESPACE,localPart = "getUsuariosRequest")
+    @ResponsePayload
+	public GetUsuariosResponse getUsuarios(@RequestPayload GetUsuariosRequest req){
+		GetUsuariosResponse res=new GetUsuariosResponse();
+		Usuario c1=new Usuario();
+		c1.setNombreCompleto("moisexio");
+		res.getUsuariosLista().add(c1);
+		return res;
+	}
 
-    
-
-   
 }
--- src/main/java/com/springsoappractice/springsoappractice/Usuario/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springsoappractice.springsoappractice.Usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-    
-}
